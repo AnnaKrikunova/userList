15bfdd61d8d269e3364b6937acf37f5124671851
Anna Krikunova <anna.krikunova99@gmail.com>
2019-08-16 14:02:40 +0300

    Updates

---
+++ precache-manifest.ef7773279beb0efe4e0dadf6821e0c19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b78fee2d2940fc110b0c9f2f9ad6e2",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "50df2c429a3c293ff591",
+    "url": "/calendar/static/css/main.f258f82e.chunk.css"
+  },
+  {
+    "revision": "00f53f095410fcfd373a",
+    "url": "/calendar/static/js/2.2ffdab0a.chunk.js"
+  },
+  {
+    "revision": "50df2c429a3c293ff591",
+    "url": "/calendar/static/js/main.de406531.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
